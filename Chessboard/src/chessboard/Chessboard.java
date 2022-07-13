060eaf900d4db3beeb8e7c2615aee5d369167cc5
Miro <Miro@DESKTOP-TDVHG99>
2022-07-13 17:15:20 +0200

    chessboard

--- Chessboard/src/chessboard/Chessboard.java
+++ Chessboard/src/chessboard/Chessboard.java
@@ -1,15 +1,21 @@
 package chessboard;
 
+//task - create chessboard from 1s and 0s
 public class Chessboard {
 	public static void main(String args[]) {
-		int[][] pole = new int[7][7];
 		int pomocna = 0;
 		
+		//loop for 8 rows 
 		for (int i = 0; i <= 7; i++) {
+			//new line after each row
 			System.out.println();
+			//add 1 so next line starts with other value
 			pomocna++;
+			
+			//loop for 8 collums
 			for (int j = 0; j <= 7; j++) {
-				System.out.print(pomocna%2 + ", ");
+				//print %2 of pomocna and space 
+				System.out.print(pomocna%2 + " ");
 				pomocna++;
 			}
 		}
